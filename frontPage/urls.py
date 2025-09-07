8c9da0f1326304c5a1f2c63591809b0f6cd38684
Jeronimo Acosta <jeronimoacosta7@gmail.com>
2025-09-07 14:46:06 -0500

    fixed typo

--- frontPage/urls.py
+++ frontPage/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
-    path ('create-vm/', views.create_vm, name='create_vm'), # ruta de crear vm
-    path('create-project/', views.create_project, name='create_project')
+    path("", views.dashboard, name="dashboard"),
+    path("create-vm/", views.create_vm, name="create_vm"),  # ruta de crear vm
+    path("create-project/", views.create_project, name="create_project"),
 ]
