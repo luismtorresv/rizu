81c01535deb2c6f73e93ae5f402211be04feb9db
Jeronimo Acosta <jeronimoacosta7@gmail.com>
2025-09-07 14:47:59 -0500

    added missing params

--- frontPage/views.py
+++ frontPage/views.py
@@ -1,42 +1,48 @@
 from django.shortcuts import render
 from Rizu import openStackCommunication
 
+
 def dashboard(request):
 
-    # Datos falsos de ejemplo 
+    # Datos falsos de ejemplo
     resources = {
-        'vms': [
-            {'name': 'fisher', 'type': 'VM instance', 'status': 'Running'},
-            {'name': 'johnson', 'type': 'VM instance', 'status': 'Stopped'},
+        "vms": [
+            {"name": "fisher", "type": "VM instance", "status": "Running"},
+            {"name": "johnson", "type": "VM instance", "status": "Stopped"},
         ],
-        'load_balancers': [
-            {'name': 'kellner', 'type': 'Load balancer', 'status': 'Active'},
+        "load_balancers": [
+            {"name": "kellner", "type": "Load balancer", "status": "Active"},
         ],
-        'databases': [
-            {'name': 'melina', 'type': 'Database', 'status': 'Available'},
-        ]
+        "databases": [
+            {"name": "melina", "type": "Database", "status": "Available"},
+        ],
     }
 
     user_info = {
-        'subnet': 'X.X.X.X/8',
-        'status': 'Running',
-        'current_account': 'Elias',
-        'role': 'Admin'
+        "subnet": "X.X.X.X/8",
+        "status": "Running",
+        "current_account": "Elias",
+        "role": "Admin",
     }
 
     context = {
-        'resources': resources,
-        'user_info': user_info,
+        "resources": resources,
+        "user_info": user_info,
     }
 
-    return render(request, 'dashboard.html', context)
+    return render(request, "dashboard.html", context)
+
 
 def create_vm(request):
-    return render(request, 'create_vm.html') 
+    return render(request, "create_vm.html")
+
 
 def create_project(request):
 
+    test_name = "project 1"
+    test_description = "this is the first test project for Rizu. Let's pray it works"
+
     conn = openStackCommunication()
-    conn.create_openstack_project()
-    
-    return render(request,'successfully created project')
+    conn.create_openstack_project(test_name, test_description)
+
+    return render(request, "successfully created project")
