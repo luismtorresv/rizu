7b44508ff11de6c3d9a428d26808c52d5fc465f2
Jeronimo Acosta <jeronimoacosta7@gmail.com>
2025-09-08 08:33:22 -0500

    removed create_project button from the html template and url. The function stays in the view for later uses

--- frontPage/urls.py
+++ frontPage/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
     path("create-vm/", views.create_vm, name="create_vm"),  # ruta de crear vm
-    path("create-project/", views.create_project, name="create_project"),
 ]
