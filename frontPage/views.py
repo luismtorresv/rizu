1cf558d377b7cf90a1138272882c44210aefd979
Luis Torres <lmtorresv@eafit.edu.co>
2025-09-08 15:20:42 -0500

    Format with `black`

--- frontPage/views.py
+++ frontPage/views.py
@@ -5,7 +5,6 @@ from Rizu.openStackCommunication import OpenStackCommunication
 
 
 def dashboard(request):
-
     # Datos falsos de ejemplo
     resources = {
         "vms": [
@@ -40,7 +39,6 @@ def create_vm(request):
 
 
 def create_project(request):
-
     test_name = "project 1"
     test_description = "this is the first test project for Rizu. Let's pray it works"
     test_username = "admin"
