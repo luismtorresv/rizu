351a248115886f34c6e452626fc867f2c7bca355
Jeronimo Acosta Acevedo <130415762+Jeronimo1303@users.noreply.github.com>
2025-09-08 11:14:16 -0500

    Add preliminary project creation feature (#34)
    * test button for creating projects
    
    * fixed typo
    
    * added missing params
    
    * added final fixes before testing
    
    * fixed module import
    
    * added ability for users to be added to a project at creation
    
    * fixed param error
    
    * removed create_project button from the html template and url. The function stays in the view for later uses

--- Rizu/openStackCommunication.py
+++ Rizu/openStackCommunication.py
@@ -1,24 +1,41 @@
 import openstack
 import os
 
+
 class OpenStackCommunication:
 
-    os.environ['OS_CLIENT_CONFIG_FILE'] = os.path.expanduser('/etc/kolla/clouds.yaml')
-    conn = openstack.connect(cloud='kolla-admin-system')
-    
+    def __init__(self, cloud="kolla-admin-system"):
+        os.environ["OS_CLIENT_CONFIG_FILE"] = os.path.expanduser(
+            "/etc/kolla/clouds.yaml"
+        )
+        self.conn = openstack.connect(cloud=cloud)
 
-    def create_openstack_project(self, project_name, project_description):
-        try: 
+    def create_openstack_project(self, project_name, project_description, username):
+        try:
 
             new_project = self.conn.identity.create_project(
-                name = project_name,
-                description = project_description,
-                domain_id = 'default',
-                enabled = True,
+                name=project_name,
+                description=project_description,
+                domain_id="default",
+                enabled=True,
             )
 
-            return True
-        except:
+            # assign myself to the project
+            self.assign_openstack_role(project_name, username)
 
+            return True
+        except Exception as e:
+
+            print(f"Something went wrong with the project creation. Error: {e}")
             return False
-            print('Something went wrong with the project creation')
+
+    def assign_openstack_role(self, project_name, username):
+
+        project = self.conn.identity.find_project(project_name)
+        user = self.conn.identity.find_user(username)
+        role = self.conn.identity.find_role("admin")
+
+        if not (project and user and role):
+            raise ValueError("Project, user, or role not found!")
+
+        self.conn.identity.assign_project_role_to_user(project, user, role)
--- frontPage/urls.py
+++ frontPage/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
-    path ('create-vm/', views.create_vm, name='create_vm'), # ruta de crear vm
+    path("", views.dashboard, name="dashboard"),
+    path("create-vm/", views.create_vm, name="create_vm"),  # ruta de crear vm
 ]
