0466c19a27edc3d071d3709b277262682a1a36d7
Luis Torres <lmtorresv@eafit.edu.co>
2025-09-08 15:20:42 -0500

    Format with `black`

--- Rizu/asgi.py
+++ Rizu/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Rizu.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Rizu.settings")
 
 application = get_asgi_application()
--- Rizu/openStackCommunication.py
+++ Rizu/openStackCommunication.py
@@ -3,7 +3,6 @@ import os
 
 
 class OpenStackCommunication:
-
     def __init__(self, cloud="kolla-admin-system"):
         os.environ["OS_CLIENT_CONFIG_FILE"] = os.path.expanduser(
             "/etc/kolla/clouds.yaml"
@@ -12,7 +11,6 @@ class OpenStackCommunication:
 
     def create_openstack_project(self, project_name, project_description, username):
         try:
-
             new_project = self.conn.identity.create_project(
                 name=project_name,
                 description=project_description,
@@ -25,12 +23,10 @@ class OpenStackCommunication:
 
             return True
         except Exception as e:
-
             print(f"Something went wrong with the project creation. Error: {e}")
             return False
 
     def assign_openstack_role(self, project_name, username):
-
         project = self.conn.identity.find_project(project_name)
         user = self.conn.identity.find_user(username)
         role = self.conn.identity.find_role("admin")
