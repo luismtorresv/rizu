a0af180bb47a55bdc3f2b68b1f03c2ee5a44f61d
Jeronimo Acosta <jeronimoacosta7@gmail.com>
2025-09-15 11:44:18 +0000

    Add dashboard application and move all the views and urls from frontPage to dashboard

--- Rizu/urls.py
+++ Rizu/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("frontPage/", include("frontPage.urls")),
+    path("dashboard/", include("dashboard.urls")),
 ]
