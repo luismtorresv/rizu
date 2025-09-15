28fac0c5b018727197d36da46d8dc1b78df099d1
Jeronimo1303 <jeronimoacosta7@gmail.com>
2025-09-15 11:52:42 +0000

    created new frontPage view

--- Rizu/urls.py
+++ Rizu/urls.py
@@ -20,5 +20,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("",include("frontPage.urls")),
     path("dashboard/", include("dashboard.urls")),
 ]
---
+++ frontPage/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.frontPage, name="front_page"),
+]
